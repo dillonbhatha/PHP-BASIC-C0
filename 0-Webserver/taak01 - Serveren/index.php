bf2c7c98651ce0554d5f3cd09a3bc197abec2493
dillon bhatha <dillon.bhatha@student.rocva.nl>
2021-12-21 11:49:21 +0100

    opdracht 2.1 t/m 2.3

--- 0-Webserver/taak01 - Serveren/index.php
+++ 0-Webserver/taak01 - Serveren/index.php
@@ -2,3 +2,25 @@
 
 
 echo "Hallo en Welkom!";
+
+$score = 0;
+
+echo $score;
+
+$naam = "Dillon";
+$name = "Mijn naam is $naam";
+
+echo $naam;
+echo $name;
+
+$leeftijd=20;
+$mijnleeftijd="Ik ben $leeftijd";
+
+echo $mijnleeftijd;
+
+$melk = "€1,00";
+$melkprijs = "de huidige prijs van een pak melk $melk";
+
+echo $melkprijs;
+
+?>
